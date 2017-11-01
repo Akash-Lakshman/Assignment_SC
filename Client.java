f11620f6d2e35771e923657c8bbfe5d857dfa931
Akash Lakshman <lakshmaa@tcd.ie>
2017-11-01 17:01:42 +0000

    Client started Connected to Server

--- Client.java
+++ Client.java
@@ -23,10 +23,42 @@ public class Client {
       inputLine = new DataInputStream(new BufferedInputStream(System.in));
 	System.out.println("Available for Connection.");
     } catch (UnknownHostException e) {
-      System.err.println(" ABC");
+      System.err.println(" HOST unknown	 ");
     } catch (IOException e) {
-      System.err.println("no connection");
+      System.err.println("Couldn't get I/O for the connection to host");
     }
 
+
+     /* opened a connection to on port 2222. */
+     
+    if (clientSocket != null && os != null && is != null) {
+      try {
+
+        //Starting the Client
+        System.out.println("The client started. Type any text. To quit it type 'Ok'.");
+        String responseLine;
+        os.println(inputLine.readLine());
+        while ((responseLine = is.readLine()) != null) {
+          System.out.println(responseLine);
+          if (responseLine.indexOf("Ok") != -1) {
+            break;
+          }
+          os.println(inputLine.readLine());
+        }
+
+        /*
+         * Close the output stream, close the input stream, close the socket.
+         */
+        os.close();
+        is.close();
+
+        clientSocket.close();
+      } catch (UnknownHostException e) {
+        System.err.println("Trying to connect to unknown host: " + e);
+      } catch (IOException e) {
+        System.err.println("IOException:  " + e);
+      }
+    }
+  }
 }
-}
+
