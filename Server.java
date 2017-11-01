359e0e2fe577a996ad65454b98559db81da0b79a
Akash Lakshman <lakshmaa@tcd.ie>
2017-11-01 15:43:37 +0000

    Listen to connection and ServerStart

--- Server.java
+++ Server.java
@@ -17,13 +17,32 @@ public class Server {
 
     /*
      * Open a server socket on port 2222. 
+     * 
      */
     try {
       echoServer = new ServerSocket(2222);
-	System.out.println("Server Socket created under port 2222");
+	System.out.println("Server Socket opened under port 2222");
     } catch (IOException e) {
       System.out.println(e);
     }
 
-}	
+    /*
+     * Create a socket object from the ServerSocket to listen to connections and accept
+     * connections.
+     */
+    System.out.println("The server started. To stop it press <CTRL><C>.");
+    try {
+      clientSocket = echoServer.accept();
+      is = new DataInputStream(clientSocket.getInputStream());
+      os = new PrintStream(clientSocket.getOutputStream());
+
+      /* As long as we receive data, echo that data back to the client. */
+      while (true) {
+        line = is.readLine();
+        os.println("From server: " + line);
+      }
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+  }
 }
