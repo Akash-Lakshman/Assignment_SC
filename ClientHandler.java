e9d85ca410a6bf0ae0d5381c95cf5bd6f1caf4bb
Akash-Lakshman <33263430+Akash-Lakshman@users.noreply.github.com>
2017-11-24 21:43:52 +0000

    Updated Client Handler.java
    leave chatroom and chat is updated as well

--- ClientHandler.java
+++ ClientHandler.java
@@ -13,14 +13,30 @@ public class ClientHandler extends Thread {
         }
 
         public void run() {
-                System.out.println("Client Writer Thread "+Thread.currentThread().getId()+ " : Created a thread for input "+s[0]);
+                System.out.println("Client Writer "+Thread.currentThread().getId()+ " : Created a thread for input "+s[0]);
                 Functions func = new Functions();
 
-                if(s[0].startsWith("JOIN_CHATROOM: ")) {
-                        System.out.println(" Start "+Thread.currentThread().getId()+" WriterThread: join chatroom ");
+                if(s[0].startsWith("JOIN_CHATROOM: "))
+                {
+                        System.out.println(" Start "+Thread.currentThread().getId()+" WriterThread: join chatroom");
                         func.processJoinMessage(s[0],s[1],s[2],s[3],os);
-                        System.out.println(" End "+Thread.currentThread().getId()+" WriterThread:  join chatroom ");
+                        System.out.println("****End "+Thread.currentThread().getId()+" WriterThread:  join chatroom");
                 }
-            }
+                        else if(s[0].startsWith("LEAVE_CHATROOM: ")) {
+                        System.out.println(" Start "+Thread.currentThread().getId()+"  WriterThread:  leave chatroom ");
+                        func.processLeaveMessage(s[0],s[1],s[2],os);
+                        return;
+                }
+                        else if(s[0].startsWith("CHAT: ")) {
+                        System.out.println(" Start  "+Thread.currentThread().getId()+" WriterThread:  chat ");
+                        func.processChatMessage(s[0],s[1],s[2],s[3],os);
+                        System.out.println(" End "+Thread.currentThread().getId()+"  WriterThread:  chat ");
+                }
+                        else if(s[0].startsWith("HELO ")) {
+                        System.out.println(" Start  "+Thread.currentThread().getId()+" WriterThread:  hello ");
+                        func.processHeloMessage(s[0],os);
+                        System.out.println(" End  "+Thread.currentThread().getId()+" WriterThread:  hello chatroom");
+                        return;
+                }
+        }
 }
-
